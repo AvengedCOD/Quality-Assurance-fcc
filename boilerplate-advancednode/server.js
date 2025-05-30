df52a7940fe0b6d71ddc79c17506124745d83a2e
AvengedCOD <whygenjustd@gmail.com>
2025-05-30 15:47:27 +0000

    feat: Updated .json for second lesson

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -6,13 +6,20 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+// my code 1
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+// end my code 1
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+// my code 2
+  res.render('index');
+// end my code 2
 });
 
 const PORT = process.env.PORT || 3000;
