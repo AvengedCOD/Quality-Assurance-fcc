8c055f5275d908f97b490c0c71ee998e8013b6c3
AvengedCOD <whygenjustd@gmail.com>
2025-06-02 18:38:19 +0000

    feat: Completed steps 5-11 for lesson 2

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // my code 
 
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const session = require('express-session');
 const { ObjectID } = require('mongodb');
 
@@ -33,24 +34,89 @@ app.use(passport.session());
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-  
-  app.route('/').get((req, res) => {
-    res.render('index', { 
-      title: 'Hello', 
-      message: 'Please log in' 
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
     });
-  });
+  }));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, null);
+  passport.deserializeUser( async (id, done) => {
+    try {
+      const doc = await myDataBase.findOne({ _id: new ObjectID(id) });
+      done(null, doc);
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  app.route('/').get((req, res) => {
+    res.render('index', { 
+      title: 'Connected to Database', 
+      message: 'Please log in',
+      showLogin: true,
+      showRegistration: true
     });
   });
 
+  app.route('/register').post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+        (err, doc) => {
+          if (err) {
+            res.redirect('/');
+          } else {
+            next(null, doc,ops[0]);
+          }
+        }
+      )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+);
+  
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+  
+  app.route('/profile').get(ensureAuthenticated, (req,res) => {
+    res.render('profile', {
+      username: req.user.username
+    });
+  });
+  
+  app.route('/logout').get((req, res) => {
+    req.logout(err => {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+  
+  app.use((req, res, next) => {
+    res.status(404)
+    .type('text')
+    .send('Not Found');
+  });
+  
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', {
@@ -59,6 +125,13 @@ myDB(async client => {
     });
   });
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
 
 // end my code 
 
