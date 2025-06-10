634919301776f8379b8b7b11c596af5f3e146a72
AvengedCOD <whygenjustd@gmail.com>
2025-06-10 17:36:21 +0000

    feat: Completed steps 14-22, finished lesson 2

--- boilerplate-advancednode/routes.js
+++ boilerplate-advancednode/routes.js
@@ -7,10 +7,11 @@ module.exports = function (app, myDataBase) {
       title: 'Connected to Database', 
       message: 'Please log in',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
-  
+
   app.route('/register').post((req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
@@ -31,31 +32,46 @@ module.exports = function (app, myDataBase) {
           }
         }
       )
-      }
-    })
-  },
-  passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res, next) => {
-      res.redirect('/profile');
     }
+  })
+},
+passport.authenticate('local', { failureRedirect: '/' }),
+(req, res, next) => {
+  res.redirect('/profile');
+}
 );
 
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), 
+  (req, res) => {
     res.redirect('/profile');
+});
+
+app.route('/auth/github').get(passport.authenticate('github')); 
+
+app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => {
+    req.session.user_id = req.user.id;
+    res.redirect('/chat');
+});
+
+app.route('/profile').get(ensureAuthenticated, (req, res) => {
+  res.render('profile', {
+    username: req.user.username
   });
-  
-  app.route('/profile').get(ensureAuthenticated, (req,res) => {
-    res.render('profile', {
-      username: req.user.username
-    });
+});
+
+app.route('/chat').get(ensureAuthenticated, (req, res) => {
+  res.render('chat', {
+    user: req.user
   });
-  
-  app.route('/logout').get((req, res) => {
-    req.logout(err => {
-      if (err) { return next(err); }
-      res.redirect('/');
-    });
+});
+
+app.route('/logout').get((req, res) => {
+  req.logout(err => {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
+});
 
   app.use((req, res, next) => {
     res.status(404)
--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -17,6 +17,15 @@ const passport = require('passport');
 const session = require('express-session');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
+const { Socket } = require('socket.io');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -25,18 +34,58 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   
   routes(app, myDataBase);
   auth(app, myDataBase);
-  
+
+  let currentUsers = 0;
+
+  io.on('connection', socket => {
+    ++currentUsers;
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { 
+        username: socket.request.user.username, 
+        message });
+    });
+    console.log('user ' + socket.request.user.username + ' connected');
+
+    socket.on('disconnect', () => {
+      --currentUsers;
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
+    console.log('user ' + socket.request.user.username + ' disconnected');
+    });
+  });
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', {
@@ -46,8 +95,19 @@ myDB(async client => {
   });
 });
 
-// end my code 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
+  // end my code 
   console.log('Listening on port ' + PORT);
 });
