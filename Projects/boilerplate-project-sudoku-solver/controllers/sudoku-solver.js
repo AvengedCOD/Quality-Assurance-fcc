53d72b2727096250d0b9fdf03cbcee7fc1f05fe9
AvengedCOD <whygenjustd@gmail.com>
2025-06-19 20:22:10 +0000

    feat: Updated a few files in project 4

--- Projects/boilerplate-project-sudoku-solver/controllers/sudoku-solver.js
+++ Projects/boilerplate-project-sudoku-solver/controllers/sudoku-solver.js
@@ -1,6 +1,11 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const regex = /^[0-9.]+$/;
+    if (!puzzleString) return 'Required field missing'; 
+    if (!regex.test(puzzleString)) return 'Invalid characters in puzzle';
+    if (puzzleString.length !== 81) return'Expected puzzle to be 81 characters long';
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
